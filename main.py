ed4754599b14a4bef8c7f36cb94b2246f781b062
Madoa5561 <81414605+Madoa5561@users.noreply.github.com>
2022-07-25 21:49:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,3 @@
-
-#WindowsでSeleniumを動かすまでの手順
-#Step１
-#Pythonをインストールします。
-#Pythonの公式サイトからPythonをダウンロードしてインストールします。
-#
-#Step２
-#Seleniumをインストールします。
-#コマンドプロンプトを立ち上げて、以下を実行します。
-#
-#pip install selenium
-#Step３
-#Seleniumからブラウザ（Chrome）を操作するためのドライバーをダウンロードし、パスを通します。
-#ChromeDriverの公式サイトからChromeDriver 73.0.3683.68をダウンロードします。
-#
-#ダウンロードしたzipファイルを解凍します。
-#
-#適当な場所にzipの中にあるexeファイルを置きます(例：C:\Program Files\chromedriver)。
-#
-#「環境変数を編集」を実行して(Winキーを押して「環境変数を編集」と検索すれば出てきます)、「Path」に上記のフォルダを追加します。
-#
-#コマンドプロンプトを起動し、以下のコマンドを実行して、起動できればOKです。
-#
-#> chromedriver
-#Starting ChromeDriver 73.0.3683.68 (47787ec04b6e38e22703e856e101e840b65afe72) on port 9515
-#Only local connections are allowed.
-#Please protect ports used by ChromeDriver and related test frameworks to prevent access by malicious code.
-
 # coding:utf-8
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -43,7 +15,7 @@ voicerlist = """0 四国めたん(あまあま)
 6 四国めたん(ツンツン)
 7 ずんだもん(ツンツン)
 
-あとは自分で書いてくれ"""
+無能過ぎて疲れて書く気力失った"""
 def yomu(outputword):
     driver = webdriver.Chrome(options=options)
     driver.get("https://voicevox.su-shiki.com/vertical/")
