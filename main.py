e851cbfa057025b9496ed0165c319b6b4f90f2ec
しょうた <81414605+Madoa5561@users.noreply.github.com>
2023-10-25 11:08:32 +0900

    Update main.py
    no debug

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import time, urllib
 options = Options()
 options.add_argument('--headless')
@@ -19,13 +20,13 @@ voicerlist = """0 四国めたん(あまあま)
 def yomu(outputword):
     driver = webdriver.Chrome(options=options)
     driver.get("https://voicevox.su-shiki.com/vertical/")
-    dropdown = driver.find_element_by_name("speaker")
+    dropdown = driver.find_element(By.NAME,"speaker")
     select = Select(dropdown)
     aaa = input("番号:")
     select.select_by_value(aaa)
     word = outputword
-    element = driver.find_element_by_xpath("/html/body/form/span/a[2]")
-    aTag    = element.find_element_by_tag_name("a")
+    element = driver.find_element(By.XPATH,"/html/body/form/span/a[2]")
+    aTag    = element.find_element(By.TAG_NAME,"a")
     url     = aTag.get_attribute("href")
     driver.quit()
     return url
